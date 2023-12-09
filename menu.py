6ab1d46ddebfc47949a73da26855718a726dfe41
Lúcio Campos <lciocampos2003@gmail.com>
2023-12-09 02:15:30 -0300

    nova fonte e imagem parcial de menu

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ from sys import exit
 from config import *
 
 def variaveis_menu():
-    global menu, jogo, opcoes_menu, selecionado_menu, estado_jogo
+    global menu, jogo, opcoes_menu, selecionado_menu, estado_jogo, image
     menu = 0
     jogo = 1
 
